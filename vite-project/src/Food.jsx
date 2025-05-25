88b8d3606907ad219970540fe06959f91fcf4701
Shakhoyat <shakoyatsujon@gmail.com>
2025-05-25 18:02:58 +0600

    add Footer and Food components; update App to include them

---
+++ vite-project/src/Food.jsx
@@ -0,0 +1,14 @@
+function Food(){
+    const food1 = "Pizza";
+    const food2 = "Burger";
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+            <li>Salad</li>
+            <li>Sushi</li>
+            <li>Ice Cream</li>
+        </ul>
+    );
+}
+export default Food;
